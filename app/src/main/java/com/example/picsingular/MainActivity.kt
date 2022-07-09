bf99acdfe30fb8a8ae1045f7536c86bf4fa4bd82
CoderWdd <CoderWdd@gmail.com>
2022-07-09 21:50:32 +0800

    component alone

--- app/src/main/java/com/example/picsingular/MainActivity.kt
+++ app/src/main/java/com/example/picsingular/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,57 +37,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyScaffold() {
     val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
     val items = listOf("图床", "社区")
 
-    var selectedItem by remember { mutableStateOf(0) }
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
-            TopAppBar(
-                title = { Text(text = "PicSingular") },
-                backgroundColor = Color.White,
-                contentColor = Color.Black,
-                elevation = 7.dp,
-                navigationIcon = {
-                    IconButton(onClick = {
-                        scope.launch {
-                            scaffoldState.drawerState.open()
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "",
-                            tint = Color.Black
-                        )
-                    }
-                }
-            )
+            MyTopBar(appTitle = "PicSingular", scaffoldState = scaffoldState)
         },
         bottomBar = {
-            BottomNavigation() {
-                items.forEachIndexed { index, item ->
-                    BottomNavigationItem(
-                        modifier = Modifier.background(MaterialTheme.colors.primaryVariant),
-                        selectedContentColor = Color.Black,
-                        selected = selectedItem == index,
-                        onClick = { selectedItem = index }, label = { Text(text = item, color = Color.Black) },
-                        icon = {
-                            when (index) {
-                                0 -> Icon(
-                                    imageVector = if (selectedItem == index) Icons.Filled.Home else Icons.Outlined.Home,
-                                    contentDescription = item,
-                                    tint = if (selectedItem == index) Color.Black else MaterialTheme.colors.secondary
-                                )
-                                1 -> Icon(
-                                    imageVector = if (selectedItem == index) Icons.Filled.Create else Icons.Outlined.Create,
-                                    contentDescription = item,
-                                    tint = if (selectedItem == index) Color.Black else MaterialTheme.colors.secondary
-                                )
-                            }
-                        })
-                }
-            }
+            MyBottomBar(items = items)
         },
         drawerContent = {
             MyDrawer()
@@ -98,9 +55,32 @@ fun MyScaffold() {
             modifier = Modifier.padding(it),
         ) {
             Text(text = "help", color = TextPrimaryColor)
-
         }
     }
+}
+
+@Composable
+fun MyTopBar(appTitle: String,scaffoldState: ScaffoldState){
+    val scope = rememberCoroutineScope()
+    TopAppBar(
+        title = { Text(text = appTitle) },
+        backgroundColor = Color.White,
+        contentColor = Color.Black,
+        elevation = 8.dp,
+        navigationIcon = {
+            IconButton(onClick = {
+                scope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "",
+                    tint = Color.Black
+                )
+            }
+        }
+    )
 }
 
 @Preview
@@ -113,6 +93,34 @@ fun MyDrawer(){
     ) {
         repeat(5) { item ->
             Text(text = "Item $item", modifier = Modifier.padding(8.dp), color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun MyBottomBar(items: List<String>){
+    var selectedItem by remember { mutableStateOf(0) }
+    BottomNavigation() {
+        items.forEachIndexed { index, item ->
+            BottomNavigationItem(
+                modifier = Modifier.background(MaterialTheme.colors.primaryVariant),
+                selectedContentColor = Color.Black,
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }, label = { Text(text = item, color = Color.Black) },
+                icon = {
+                    when (index) {
+                        0 -> Icon(
+                            imageVector = if (selectedItem == index) Icons.Filled.Home else Icons.Outlined.Home,
+                            contentDescription = item,
+                            tint = if (selectedItem == index) Color.Black else MaterialTheme.colors.secondary
+                        )
+                        1 -> Icon(
+                            imageVector = if (selectedItem == index) Icons.Filled.Create else Icons.Outlined.Create,
+                            contentDescription = item,
+                            tint = if (selectedItem == index) Color.Black else MaterialTheme.colors.secondary
+                        )
+                    }
+                })
         }
     }
 }
