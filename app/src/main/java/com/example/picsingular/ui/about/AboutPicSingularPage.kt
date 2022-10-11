92f7ac8f3455f63478bc63436ebe3505ceda9177
Lorde <rioaformyking@gmail.com>
2022-10-11 20:56:09 +0800

    feat: add about page and setting

--- app/src/main/java/com/example/picsingular/ui/about/AboutPicSingularPage.kt
+++ app/src/main/java/com/example/picsingular/ui/about/AboutPicSingularPage.kt
@@ -1,9 +1,16 @@
 package com.example.picsingular.ui.about
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 
 @Composable
 fun AboutPicSingularPage(navHostController: NavHostController){
-
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "About me", color= Color.Black)
+    }
 }
