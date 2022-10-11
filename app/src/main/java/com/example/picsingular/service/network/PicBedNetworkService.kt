fc4057f40172ce52ee3bf0895c471c8294094ea0
Rioa <rioaformyking@gmail.com>
2022-10-11 14:32:47 +0800

    feat: add image api service

--- app/src/main/java/com/example/picsingular/service/network/PicBedNetworkService.kt
+++ app/src/main/java/com/example/picsingular/service/network/PicBedNetworkService.kt
@@ -8,5 +8,9 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface PicBedNetworkService {
+    @POST("images/upload")
+    suspend fun uploadImagesToPicBed(@Body multipartFileList: MultipartBody): RetrofitResponseBody<List<String>>
 
+    @GET("list/{page}/{size}")
+    suspend fun getPicBedImagesList(@Path("page") page: Int, @Path("size") size: Int): RetrofitResponseBody<List<String>>
 }
